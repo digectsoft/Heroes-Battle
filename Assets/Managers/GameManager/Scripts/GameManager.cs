8366accb7e0412d88b336edc0d87c756d2c7ff25
Andrew Peresko <andrewperesko@yahoo.com>
2025-01-18 18:34:00 +0300

    UI.

--- Assets/Managers/GameManager/Scripts/GameManager.cs
+++ Assets/Managers/GameManager/Scripts/GameManager.cs
@@ -1,5 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
+// ---------------------------------------------------------------------------
+// Copyright (c) 2025 Andrew Peresko
+//
+// Licensed under the Creative Commons Attribution-NonCommercial 4.0 
+// International License (CC BY-NC 4.0). You may not use this file 
+// except in compliance with the License. To view a copy of this license,
+// visit https://creativecommons.org/licenses/by-nc/4.0/.
+//
+// You are free to:
+// - Share: Copy and redistribute the material in any medium or format.
+// - Adapt: Remix, transform, and build upon the material.
+//
+// Under the following terms:
+// - Attribution: You must give appropriate credit, provide a link to the license,
+//   and indicate if changes were made. You may do so in any reasonable manner, but 
+//   not in any way that suggests the licensor endorses you or your use.
+// - NonCommercial: You may not use the material for commercial purposes.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE, AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT, OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+// ---------------------------------------------------------------------------
+
 using DG.Tweening;
 using UnityEngine;
 
@@ -10,13 +35,11 @@ public class GameManager : MonoBehaviour
 		DOTween.Init(true, true, LogBehaviour.Verbose);
 	}
 	
-	// Start is called before the first frame update
 	void Start()
 	{
 		
 	}
 
-	// Update is called once per frame
 	void Update()
 	{
 		
